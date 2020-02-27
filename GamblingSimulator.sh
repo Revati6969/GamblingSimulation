a377809ace8fb89c18da1acb804fd7ee13546d6d
Revati Tekale <revitekale1910@gmail.com>
2020-02-27 15:37:04 +0530

    ADDED:Maked  bet winner or looser

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -5,3 +5,22 @@ echo "Welcome to Gambling Simulator Program"
 #Constants
 STAKE=100
 BET=1 
+
+dailyBetAmount=$STAKE
+
+function betting()
+{
+	random=$((RANDOM%2))
+
+	if [[ $random -eq 1 ]]
+	then
+		echo "you won 1 bet"
+		((dailyBetAmount++))
+	else
+		echo "you loose 1 bet"
+		((dailyBetAmount--))
+	fi
+	echo $dailyBetAmount
+}
+betting
+
