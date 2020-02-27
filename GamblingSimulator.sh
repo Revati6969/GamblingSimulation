fc6654524e7c440a073dbd8784e333b94799c618
Revati Tekale <revitekale1910@gmail.com>
2020-02-27 16:37:01 +0530

    ADDED:Resigned after reaching daily limit

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -8,19 +8,38 @@ BET=1
 
 dailyBetAmount=$STAKE
 
+limit()
+{
+	maxLimit=$(($dailyBetAmount+$dailyBetAmount*50/100))
+	minLimit=$(($dailyBetAmount-$dailyBetAmount*50/100))
+}
+
 function betting()
 {
-	random=$((RANDOM%2))
+	while [[ $dailyBetAmount -ne 0 ]]
+	do
+		random=$((RANDOM%2))
+		if [[ $random -eq 1 ]]
+		then
+			dailyBetAmount=$(( $dailyBetAmount + $BET ))
+			if [[ $dailyBetAmount -eq $maxLimit ]]
+			then
+				echo "you have reached maximum limit"
+				break
+			fi
+		else
+			dailyBetAmount=$(( $dailyBetAmount - $BET ))
+			if [[ $dailyBetAmount -eq $minLimit ]]
+			then
+				echo "you have reached minimum limit"
+				break
+			fi
+		fi
+	done
 
-	if [[ $random -eq 1 ]]
-	then
-		echo "you won 1 bet"
-		((dailyBetAmount++))
-	else
-		echo "you loose 1 bet"
-		((dailyBetAmount--))
-	fi
 	echo $dailyBetAmount
 }
+
+limit
 betting
 
