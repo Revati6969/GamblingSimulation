29b8983e0d40649a9414e2d4dce4b1000033da2a
Revati Tekale <revitekale1910@gmail.com>
2020-02-28 18:02:04 +0530

    ADDED:Found lucky and unlucky day

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -3,15 +3,15 @@
 STAKE=100
 maxLimit=0
 minLimit=0
-totalWin=0
-totalLoss=0
-winAmount=0
-
-read -p "Enter the days : " DAYS
+DAYS=20
+totalStake=0
+VALUE=50
 
 dailyBetAmount=$STAKE
+declare -A Dictionary
 
-function limit () {
+function limit () 
+{
    maxLimit=$(($dailyBetAmount+$dailyBetAmount*50/100))
    minLimit=$(($dailyBetAmount-$dailyBetAmount*50/100))
 }
@@ -20,41 +20,60 @@ function betting()
 {
    for (( counter=1; counter<=$DAYS; counter++ ))
    do
-      dailyBetAmount=100
-      totalWin=0
-      totalLoss=0
       while [[ $dailyBetAmount -ne 0 ]]	
       do
-	random=$((RANDOM%2))
-	if [ $random -eq 1 ]
-	then 
-	    ((dailyBetAmount++))
-	    if [ $dailyBetAmount -eq $maxLimit ]
-	    then 
-		break
-        fi
-        else
-	    ((dailyBetAmount--))
-	    if [ $dailyBetAmount -eq $minLimit ]
-	    then 
-                break
+         random=$((RANDOM%2))
+         if [ $random -eq 1 ]
+         then 
+            ((dailyBetAmount++))
+            if [ $dailyBetAmount -eq $maxLimit ]
+            then 
+               break
             fi
-        fi
-       done
+         else
+            ((dailyBetAmount--))
+            if [ $dailyBetAmount -eq $minLimit ]
+            then 
+               break
+            fi
+         fi
+      done
 
-       if [ $dailyBetAmount -eq $maxLimit ]
-       then
-           totalWin=$(($maxLimit-$STAKE))
-           winAmount=$(($winAmount+$totalWin))
-       else
-           totalLoss=$(($STAKE-$minLimit))
-           looseAmount=$(($looseAmount+50))
-       fi
-       echo "day: $counter  Amount: $dailyBetAmount  Win: $totalWin  Loss:$totalLoss"
+      if [ $dailyBetAmount -eq $minLimit ]
+      then
+         totalStake=$(($totalStake-$VALUE))
+         Dictionary[$counter]=$totalStake
+      else
+         totalStake=$(($totalStake+$VALUE))
+         Dictionary[$counter]=$totalStake
+      fi
+      dailyBetAmount=100
 done
- echo $winAmount
-echo $looseAmount
+echo "totalStake: $totalStake"
 }
  
 limit
 betting
+
+echo "your Stakes: " $totalStake
+
+profitValue=$(printf "%s\n" ${Dictionary[@]} | sort -n | tail -1 )
+lossValue=$(printf "%s\n" ${Dictionary[@]} | sort -n | head -1 )
+
+echo profitValue: $profitValue
+echo lossValue :$lossValue
+
+echo "day: " ${!Dictionary[@]} 
+echo "stake: "${Dictionary[@]}
+for key in ${!Dictionary[@]}
+do
+   if [[ ${Dictionary[$key]} -eq $profitValue ]]
+   then
+      echo "lucky day :$key"
+   fi
+
+   if [[ ${Dictionary[$key]} -eq $lossValue ]]
+   then
+      echo "unlucky day :$key"
+   fi
+done
