1d9f4d1d689d1d1e5dae570943f519779843876e
Revati Tekale <revitekale1910@gmail.com>
2020-02-28 16:14:58 +0530

    ADDED:EveryDayAmount

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,45 +1,54 @@
 #!/bin/bash -x
 
-echo "Welcome to Gambling Simulator Program"
-
-#Constants
 STAKE=100
-BET=1 
+maxLimit=0
+minLimit=0
+DAYS=20
+totalWin=0
+totalLoss=0
 
 dailyBetAmount=$STAKE
 
-limit()
-{
-	maxLimit=$(($dailyBetAmount+$dailyBetAmount*50/100))
-	minLimit=$(($dailyBetAmount-$dailyBetAmount*50/100))
+function limit () {
+   maxLimit=$(($dailyBetAmount+$dailyBetAmount*50/100))
+   minLimit=$(($dailyBetAmount-$dailyBetAmount*50/100))
 }
 
 function betting()
 {
-	while [[ $dailyBetAmount -ne 0 ]]
-	do
-		random=$((RANDOM%2))
-		if [[ $random -eq 1 ]]
-		then
-			dailyBetAmount=$(( $dailyBetAmount + $BET ))
-			if [[ $dailyBetAmount -eq $maxLimit ]]
-			then
-				echo "you have reached maximum limit"
-				break
-			fi
-		else
-			dailyBetAmount=$(( $dailyBetAmount - $BET ))
-			if [[ $dailyBetAmount -eq $minLimit ]]
-			then
-				echo "you have reached minimum limit"
-				break
-			fi
-		fi
-	done
+   for (( counter=1; counter<=$DAYS; counter++ ))
+   do
+      dailyBetAmount=100
+      totalWin=0
+      totalLoss=0
+      while [[ $dailyBetAmount -ne 0 ]]	
+      do
+	random=$((RANDOM%2))
+	if [ $random -eq 1 ]
+	then 
+	    ((dailyBetAmount++))
+	    if [ $dailyBetAmount -eq $maxLimit ]
+	    then 
+		break
+        fi
+        else
+	    ((dailyBetAmount--))
+	    if [ $dailyBetAmount -eq $minLimit ]
+	    then 
+                break
+            fi
+        fi
+       done
 
-	echo $dailyBetAmount
+       if [ $dailyBetAmount -eq $maxLimit ]
+       then
+           totalWin=$(($maxLimit-$STAKE))
+       else
+           totalLoss=$(($STAKE-$minLimit))
+       fi
+       echo "day: $counter  Amount: $dailyBetAmount  Win: $totalWin  Loss:$totalLoss"
+done
 }
-
+ 
 limit
 betting
-
